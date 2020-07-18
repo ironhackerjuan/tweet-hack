33a8da9ac63192eb4f9ce21c5028aca3672a4ef2
Carol Alonso <c.alonsolp@gmail.com>
2020-07-18 14:47:00 +0200

    fix: likes with POST

--- config/routes.js
+++ config/routes.js
@@ -15,5 +15,5 @@ router.post('/users', sessionMiddleware.isNotAuthenticated, uploads.single('avat
 router.get('/activate/:token', sessionMiddleware.isNotAuthenticated, usersController.activateUser);
 router.post('/logout', sessionMiddleware.isAuthenticated, usersController.logout);
 router.get('/tweets', sessionMiddleware.isAuthenticated, tweetsController.list);
-router.get('/tweets/:id/like', sessionMiddleware.isAuthenticated, tweetsController.like)
+router.post('/tweets/:id/like', sessionMiddleware.isAuthenticated, tweetsController.like)
 module.exports = router;
--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 function like(e) {
 	const button = e.currentTarget;
-	axios.get(`http://localhost:3000/tweets/${button.id}/like`)
+	axios.post(`http://localhost:3000/tweets/${button.id}/like`)
 		.then(res => {
 			const add = res.data.like;
 			button.querySelector('.likes-count').innerText = Number(button.querySelector('.likes-count').innerText) + add;
